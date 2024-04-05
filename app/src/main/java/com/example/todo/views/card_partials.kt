232333a30df3f0fd9917c31dfd07614f86e76332
syasul <githubku4@gmail.com>
2024-04-06 03:14:59 +0700

    add

---
+++ app/src/main/java/com/example/todo/views/card_partials.kt
@@ -0,0 +1,12 @@
+package com.example.todo.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todo.R
+
+class card_partials : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_partials)
+    }
+}
